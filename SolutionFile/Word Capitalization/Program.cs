05022fc24541c11d739c32646ac78a6b34daae64
md_rajib_rari <rajibhossen688@gmail.com>
2023-04-13 18:06:30 +0600

    lacky number

---
+++ SolutionFile/Word Capitalization/Program.cs
@@ -0,0 +1,17 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+       string str = UppercaseFirst(Console.ReadLine());
+       Console.WriteLine(str);
+    }
+    static string UppercaseFirst(string s)
+    {    
+        if (string.IsNullOrEmpty(s))
+        {
+            return string.Empty;
+        }      
+        return char.ToUpper(s[0]) + s.Substring(1);
+    }
+}
