c51c6b122294180a9aba59092d6ff64c5d8e2d40
md_rajib_rari <rajibhossen688@gmail.com>
2023-04-14 12:36:59 +0600

    registion system

---
+++ SolutionFile/C Registration system/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using NHibernate.Mapping;
+using System.Configuration;
+using System.Linq;
+
+int n = int.Parse(Console.ReadLine());
+
+Dictionary<string, int> database = new Dictionary<string, int>();
+
+string s;
+while (n<0)
+{
+    s = Console.ReadLine();
+
+    if (database.Count() == 0)
+    {
+        Console.WriteLine("OK");
+        database[s] = 1;
+    }
+    else
+    {
+        Console.WriteLine($"{s}" + database[s]);       
+        database[s] += 1;
+    }
+}
