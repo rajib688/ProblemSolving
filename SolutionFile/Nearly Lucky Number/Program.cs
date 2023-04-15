2753b316ac4b9c7ac72f65d156edd4071f8199b3
md_rajib_rari <rajibhossen688@gmail.com>
2023-04-15 21:44:41 +0600

    Nearly Lucky Number

---
+++ SolutionFile/Nearly Lucky Number/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+long n = Int64.Parse(Console.ReadLine());
+int Count = 0;
+while (n!=0)
+{
+	if (n%10 == 4 || n%10 == 7)
+	{
+		Count = Count + 1;
+	}
+	n = n / 10;
+}
+if (Count == 4 || Count == 7 )
+{
+	Console.WriteLine("YES");
+}
+else
+{
+	Console.WriteLine("NO");	
+}
