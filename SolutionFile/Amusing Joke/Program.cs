75b65f55e004291ce030be2bf656afdd0c69b2e8
md_rajib_rari <rajibhossen688@gmail.com>
2023-04-20 20:09:09 +0600

    solve

---
+++ SolutionFile/Amusing Joke/Program.cs
@@ -0,0 +1,8 @@
+﻿string a, b, c, s;
+a = Console.ReadLine();
+b = Console.ReadLine();
+c = Console.ReadLine();
+
+s = a + b;
+
+//Array.Sort(s.StartsWith(), s.EndsWith());
---
+++ SolutionFile/In Search of an EasyProblem/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int n, Count = 0;
+n = int.Parse(Console.ReadLine());
+int[] a = new int[n];
+string userinput = Console.ReadLine();
+var inputitem = userinput.Split(' ');
+for (int i = 0; i < n; i++)
+{
+    a[i] = int.Parse(inputitem[i]);
+    if (a[i] == 1)
+    {
+        Count++;
+    }
+}
+if (Count == 0)
+{
+    Console.WriteLine("EASY");
+}
+else
+{
+    Console.WriteLine("HARD");
+}
