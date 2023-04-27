e2d1772b19c73844293eb78d1a1b9736fd642ece
md_rajib_rari <rajibhossen688@gmail.com>
2023-04-28 00:17:29 +0600

    yes to yes

---
+++ SolutionFile/YES or YES/Program.cs
@@ -0,0 +1,27 @@
+﻿int n = int.Parse(Console.ReadLine());
+while(n>0)
+{
+    string s = Console.ReadLine();
+    int A = 0;
+    if (s[0] == 'y' || s[0] == 'Y')
+    {
+        A++;
+    }
+    if (s[1] == 'e' || s[1] == 'E')
+    {
+        A++;
+    }
+    if (s[2] == 's' || s[2] == 'S')
+    {
+        A++;
+    }
+    if (A==3)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+    n--;
+}
